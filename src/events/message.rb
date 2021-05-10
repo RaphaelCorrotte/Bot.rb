ac5e5fb63109f4c54824cb64a2aa341231494c7b
senchuu <senchuuuu@gmail.com>
2021-05-10 18:51:14 +0200

    Update the message event, add the error manager

--- src/events/message.rb
+++ src/events/message.rb
@@ -6,12 +6,20 @@ module DiscordBot
       $client.message(start_with: $client.config[:prefix]) do |event|
         args = event.content.slice($client.config[:prefix].size, event.content.size).split(" ")
         next unless event.content.start_with?($client.config[:prefix])
-        if $client.commands.include?(args[0])
-          DiscordBot::Commands.method(args[0]).call(event)[:run].call
+        name = args.shift
+        if DiscordBot::Utils::get_command(name, { :boolean => true })
+          begin
+            command = DiscordBot::Utils::get_command(name)
+            command.run.call(event, { :args => args })
+          rescue => e
+            CONSOLE_LOGGER.error("Error running #{name}: #{e.message}")
+            FILE_LOGGER.write(e.message, :errors)
+          else
+            CONSOLE_LOGGER.info("Command #{name} executed")
+          end
         end
       end
     end
     module_function :message
   end
 end
-
